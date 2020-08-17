54635a8b932ec0b3b52b73c6259119035306276b
seanaleid <seanaleid@gmail.com>
2020-08-17 18:18:12 -0400

    Print 8 and multiply 8*9 function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,36 +7,67 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # RAM --> random access memory --> 8 bit
+        self.ram = [0]*256
+        # program counter --> counter
+        self.pc = 0
+        # register --> local variable holders
+        self.register = [0]*8
+        # if the program is running
+        self.running = True
+
+    def ram_read(self, address):
+        # look in the RAM at the address passed through 
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        # takes the address and updates the value at that address
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
+        filename = sys.argv[1]
+        with open(filename) as f:
+            for line in f:
+                line = line.split('#')
+                try:
+                    v = int(line[0], 2)
+                except ValueError:
+                    continue
+                # print(v)
+                self.ram[address] = v
+                address+=1
+        print(self.ram[:15])
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        #elif op == "SUB": etc
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.register[reg_a] += self.register[reg_b]
+        elif op == "SUB":
+            self.register[reg_a] -= self.register[reg_b]
+        elif op == 'MUL':
+            self.register[reg_a] *= self.register[reg_b]
+        elif op == "ADIVDD":
+            self.register[reg_a] /= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +93,36 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        commands = {
+            'LDI': 0b10000010,
+            'PRN': 0b01000111,
+            'HLT': 0b00000001,
+            'MUL': 0b10100010
+        }
+
+        while self.running:
+
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+
+            if IR == commands['HLT']:
+                print(f"HALTED")
+                self.running = False
+            elif IR == commands['LDI']:
+                address = operand_a
+                value = operand_b
+                self.register[address] = value
+                print(f'REGISTER {self.register}')
+                self.pc+=3
+            elif IR == commands['PRN']:
+                address = operand_a
+                value = self.register[address]
+                print(value)
+                self.pc+=2
+            elif IR == commands['MUL']:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc+=3
+            else:
+                print(f'NOT KNOWN {IR} AT ADDRESS {self.pc}')
+                sys.exit(1)
